b5979ce289e8732a4ae389daa360652a43d6c0c3
macbook <akrame0@gmail.com>
2026-09-24 07:09:54 +0100

    Remove the short-FFT analyser name, now that every port reads WebAudioAnalyser

--- kiteplayer-audioviz/src/commonMain/kotlin/io/github/yuroyami/kiteplayer/audioviz/viz/ShortFftAnalyser.kt
+++
@@ -1,5 +0,0 @@
-package io.github.yuroyami.kiteplayer.audioviz.viz
-
-/** The earlier name of [WebAudioAnalyser] for pages with a short FFT. Both names are one class now. */
-@Deprecated("WebAudioAnalyser adds up the page's bin shape for every FFT size.", ReplaceWith("WebAudioAnalyser"))
-internal typealias ShortFftAnalyser = WebAudioAnalyser
